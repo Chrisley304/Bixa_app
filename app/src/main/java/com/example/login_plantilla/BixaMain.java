5ba1eb2ecac644a7b0f6a272584fc07c906e9b1e
Christian Leyva <53919610+Chrisley304@users.noreply.github.com>
2021-02-03 11:25:57 -0600

    Drawer funcionando beta 1, y agregue builder de alerta al intentar cerrar sesion.

--- app/src/main/java/com/example/login_plantilla/BixaMain.java
+++ app/src/main/java/com/example/login_plantilla/BixaMain.java
@@ -1,5 +1,6 @@
 package com.example.login_plantilla;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
@@ -9,6 +10,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,6 +20,8 @@ import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -29,13 +33,14 @@ import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.Locale;
+
 import Mensajes.ListaMensajes;
 import Mensajes.Mensaje;
 import Usuarios.Bixa;
 import Usuarios.Usuario;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class BixaMain extends AppCompatActivity {
+public class BixaMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     // Variable final requerida para el Speach to text
     private static final int REQ_CODE_SPEECH_INPUT = 100;
 
@@ -59,7 +64,7 @@ public class BixaMain extends AppCompatActivity {
     Bixa bixa = new Bixa();
 
     // Para la personalizacion de los menus
-    CircleImageView BotonFotoperfil,fotopfDrawer;
+    CircleImageView BotonFotoperfil, fotopfDrawer;
     TextView nombreNavbar;
     TextView usernameNavbar;
 
@@ -68,6 +73,10 @@ public class BixaMain extends AppCompatActivity {
         setContentView(R.layout.bixa_main);
 
         // Inicializacion de variables:
+        // Se obtiene al usuario de la actividad anterior
+        username = getIntent().getStringExtra("Usuario");
+        user = BienvenidaActivity.UsuariosRegistrados.get(username);
+        String nombreCompleto = user.getNombre() + " " + user.getApellido();
 
         // Navbar
         dwly = findViewById(R.id.DrawerLayout);
@@ -76,38 +85,47 @@ public class BixaMain extends AppCompatActivity {
         nombreNavbar = findViewById(R.id.Nombre_navbar);
         usernameNavbar = findViewById(R.id.Username_navbar);
         barra_herramientas = findViewById(R.id.toolbar);
+        navView = findViewById(R.id.nav_view);
 
-        // Actividad Principal (Bixa)
-        Boton_Enviar = findViewById(R.id.BotonEnviarMens);
-        Texto_porEnviar = findViewById(R.id.Edittxt_mensaje);
-        BotonHablar = findViewById(R.id.BotonSpeachtt);
-
-        // Accion nesesaria para la barra de herramientas
-//        setSupportActionBar(barra_herramientas);
-
-        // Para el menu despegable:
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,dwly,barra_herramientas,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        /* Para el menu despegable:  ---------------------------------*/
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dwly, barra_herramientas, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         dwly.addDrawerListener(toggle);
         toggle.syncState();
-
-        username = getIntent().getStringExtra("Usuario");
-        user = BienvenidaActivity.UsuariosRegistrados.get(username);
-        String nombreCompleto = user.getNombre() + " " + user.getApellido();
+        navView.bringToFront();
+        navView.setNavigationItemSelectedListener(this);
+        // Muestra como seleccionado por defecto la opcion de asistente del meu despegable
+        navView.setCheckedItem(R.id.nav_bixa);
+        // Oculta opciones de administrador a personas no admin:
+        if(!user.getUsername().equals("admin")){
+            Menu menu = navView.getMenu();
+            menu.getItem(R.id.nav_admin_Registros).setVisible(false);
+        }
         /*nombreNavbar.setText(nombreCompleto);
         usernameNavbar.setText(username);*/
-
         // Si el usuario agrego foto de perfil, se colocara
-        if (user.getRuta_fotoperfil() != null){
+
+        if (user.getRuta_fotoperfil() != null) {
             BotonFotoperfil.setImageURI(Uri.fromFile(user.getRuta_fotoperfil()));
             fotopfDrawer.setImageURI(Uri.fromFile(user.getRuta_fotoperfil()));
         }
 
+        BotonFotoperfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BixaMain.this, "En construccion", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        /* Actividad Principal (Bixa) ----------------------------------- */
+        Boton_Enviar = findViewById(R.id.BotonEnviarMens);
+        Texto_porEnviar = findViewById(R.id.Edittxt_mensaje);
+        BotonHablar = findViewById(R.id.BotonSpeachtt);
 
         // Inicializacion Objeto para el Text to Speach (Voz del Asistente)
         VozBixa = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if ( status == TextToSpeech.SUCCESS){
+                if (status == TextToSpeech.SUCCESS) {
                     // Seleccion del idioma
                     int idioma = VozBixa.setLanguage(Locale.getDefault());
                 }
@@ -124,67 +142,63 @@ public class BixaMain extends AppCompatActivity {
             }
         });
 
-        BotonFotoperfil.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BixaMain.this,"En construccion",Toast.LENGTH_SHORT).show();
-            }
-        });
-
         // Muestra el Boton de enviar solo cuando hay texto, para asi mostrar el boton de Speach to Text
         Texto_porEnviar.addTextChangedListener(new TextWatcher() {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.toString().equals("")){
+                if (s.toString().equals("")) {
                     Boton_Enviar.setVisibility(View.GONE);
                     BotonHablar.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     Boton_Enviar.setVisibility(View.VISIBLE);
                     BotonHablar.setVisibility(View.GONE);
                 }
             }
 
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
 
             @Override
-            public void afterTextChanged(Editable s) {}
+            public void afterTextChanged(Editable s) {
+            }
         });
 
-
+        // Boton enviar texto
         Boton_Enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String mensaje = Texto_porEnviar.getText().toString();
 
-                if(!mensaje.isEmpty()){
-                    MandarPeticion(mensaje,user);
+                if (!mensaje.isEmpty()) {
+                    MandarPeticion(mensaje, user);
 
-                }else {
-                    Toast.makeText(BixaMain.this,"Por favor ingresa texto",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(BixaMain.this, "Por favor ingresa texto", Toast.LENGTH_SHORT).show();
                 }
                 // 'Vacia' el Edit Text al enviar un mensaje
                 Texto_porEnviar.setText("");
             }
         });
 
+        /* Parte de la "mensajeria" entre bixa y el usuario */
         mMessageRecycler = (RecyclerView) findViewById(R.id.recycler_gchat);
-        mMessageRecycler.setHasFixedSize(true );
+        mMessageRecycler.setHasFixedSize(true);
         LinearLayoutManager linL = new LinearLayoutManager(this);
 //        linL.setStackFromEnd(true);
         mMessageRecycler.setLayoutManager(linL);
     }
 
-    private void MandarPeticion(String petUsuario, Usuario Perfil_usuario){
-        EnviarMensaje(petUsuario,Perfil_usuario);
+    private void MandarPeticion(String petUsuario, Usuario Perfil_usuario) {
+        EnviarMensaje(petUsuario, Perfil_usuario);
         petUsuario = petUsuario.toLowerCase();
-        String respuesta_bixa = bixa.Responder(petUsuario,Perfil_usuario,getApplicationContext());
-        EnviarMensaje(respuesta_bixa,bixa);
-        VozBixa.speak(respuesta_bixa,TextToSpeech.QUEUE_FLUSH,null);
+        String respuesta_bixa = bixa.Responder(petUsuario, Perfil_usuario, getApplicationContext());
+        EnviarMensaje(respuesta_bixa, bixa);
+        VozBixa.speak(respuesta_bixa, TextToSpeech.QUEUE_FLUSH, null);
     }
 
-    private void EnviarMensaje(String mensaje, Usuario emisor){
-        messageList.add(new Mensaje(mensaje,emisor,System.currentTimeMillis()));
+    private void EnviarMensaje(String mensaje, Usuario emisor) {
+        messageList.add(new Mensaje(mensaje, emisor, System.currentTimeMillis()));
         mMessageAdapter = new ListaMensajes(this, messageList);
         mMessageRecycler.setAdapter(mMessageAdapter);
     }
@@ -192,14 +206,14 @@ public class BixaMain extends AppCompatActivity {
     private void In_EntradaVoz() {
         // Itent -> clase de Google con funciones chidas, como reconocimiento de voz
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         // Se obtiene el lenguaje por defecto con Locale -> Clase para acceder a datos del Cel
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Bienvenido al very early build de Bixa Assistant\nPor favor, pulsa el microfono para hablar");
-        try{
-            startActivityForResult(intent,REQ_CODE_SPEECH_INPUT);
-        }catch (ActivityNotFoundException e){
-            Toast.makeText(this, "Error: " + e.getMessage(),Toast.LENGTH_SHORT).show();
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Por favor, pulsa el microfono para hablar");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -211,21 +225,21 @@ public class BixaMain extends AppCompatActivity {
             if (resultCode == RESULT_OK && data != null) {
                 ArrayList<String> resultado = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                 String salida = resultado.get(0);
-                MandarPeticion(salida,user);
+                MandarPeticion(salida, user);
             }
         }
     }
 
     @Override
     public void onBackPressed() {
-        if(dwly.isDrawerOpen(GravityCompat.START)){
+        if (dwly.isDrawerOpen(GravityCompat.START)) {
             dwly.closeDrawer(GravityCompat.START);
-        }else {
+        } else {
             ClickCerrarSesion();
         }
     }
 
-    public void ClickCerrarSesion(){
+    public void ClickCerrarSesion() {
         // Se redirige a la actividad de Editat Perfil
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Cerrar Sesion");
@@ -246,4 +260,39 @@ public class BixaMain extends AppCompatActivity {
         builder.show();
     }
 
+    //Cambio de actividad con el menu:
+    // Hace que se quede la seleccion en el menu, para indicar en que parte de la app estas
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            // Caso asistente (Actividad Actual)
+            case R.id.nav_bixa: {
+            }break;
+
+            // Caso mas informacion
+            case R.id.nav_about: {
+                Intent about = new Intent(BixaMain.this, SobrelaApp.class);
+                about.putExtra("Usuario",username);
+                startActivity(about);
+                finish();
+            }break;
+
+            // Caso editar perfil
+            case R.id.nav_editPerf:{
+                Intent edPerf = new Intent(BixaMain.this, EditarPerfil.class);
+                edPerf.putExtra("Usuario",username);
+                startActivity(edPerf);
+                finish();
+            }break;
+
+            // Caso cerrar sesion:
+            case R.id.nav_logout:{
+                ClickCerrarSesion();
+            }
+        }
+        // CIerra el menu despegable al seleccionar alguna opcion
+        dwly.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/login_plantilla/SobrelaApp.java
@@ -0,0 +1,145 @@
+package com.example.login_plantilla;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+import Usuarios.Usuario;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SobrelaApp extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Variables para controlar el menu despegable
+    DrawerLayout dwly;
+    Toolbar barra_herramientas;
+    NavigationView navView;
+
+    // Para la personalizacion de los menus
+    CircleImageView BotonFotoperfil, fotopfDrawer;
+    TextView nombreNavbar;
+    TextView usernameNavbar;
+    Usuario user;
+    String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobrela_app);
+
+        username = getIntent().getStringExtra("Usuario");
+        user = BienvenidaActivity.UsuariosRegistrados.get(username);
+        String nombreCompleto = user.getNombre() + " " + user.getApellido();
+
+        // Navbar
+        dwly = findViewById(R.id.DrawerLayout);
+        BotonFotoperfil = findViewById(R.id.BotonNavDrawer);
+        fotopfDrawer = findViewById(R.id.FotoPerfilDrawer);
+        nombreNavbar = findViewById(R.id.Nombre_navbar);
+        usernameNavbar = findViewById(R.id.Username_navbar);
+        barra_herramientas = findViewById(R.id.toolbar);
+        navView = findViewById(R.id.nav_view);
+
+        /* Para el menu despegable:  ---------------------------------*/
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dwly, barra_herramientas, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dwly.addDrawerListener(toggle);
+        toggle.syncState();
+        navView.bringToFront();
+        navView.setNavigationItemSelectedListener(this);
+        // Muestra como seleccionado por defecto la opcion de asistente del meu despegable
+        navView.setCheckedItem(R.id.nav_about);
+        // Oculta opciones de administrador a personas no admin:
+        if(!user.getUsername().equals("admin")){
+            Menu menu = navView.getMenu();
+            menu.getItem(R.id.nav_admin_Registros).setVisible(false);
+        }
+        /*nombreNavbar.setText(nombreCompleto);
+        usernameNavbar.setText(username);*/
+        // Si el usuario agrego foto de perfil, se colocara
+
+        if (user.getRuta_fotoperfil() != null) {
+            BotonFotoperfil.setImageURI(Uri.fromFile(user.getRuta_fotoperfil()));
+            fotopfDrawer.setImageURI(Uri.fromFile(user.getRuta_fotoperfil()));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (dwly.isDrawerOpen(GravityCompat.START)) {
+            dwly.closeDrawer(GravityCompat.START);
+        } else {
+            ClickCerrarSesion();
+        }
+    }
+
+    public void ClickCerrarSesion() {
+        // Se redirige a la actividad de Editat Perfil
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Cerrar Sesion");
+        builder.setMessage("¿Estas seguro que quieres cerrar sesión?, Esto te devolvera a la pantalla de inicio");
+        // Boton para cerrar sesion
+        builder.setPositiveButton("Cerrar Sesión", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    //Cambio de actividad con el menu:
+    // Hace que se quede la seleccion en el menu, para indicar en que parte de la app estas
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            // Caso asistente (Actividad Actual)
+            case R.id.nav_about: {
+            }break;
+
+            // Caso mas informacion
+            case R.id.nav_bixa: {
+                Intent bixa = new Intent(SobrelaApp.this, BixaMain.class);
+                bixa.putExtra("Usuario",username);
+                startActivity(bixa);
+                finish();
+            }break;
+
+            // Caso editar perfil
+            case R.id.nav_editPerf:{
+                Intent edPerf = new Intent(SobrelaApp.this, EditarPerfil.class);
+                edPerf.putExtra("Usuario",username);
+                startActivity(edPerf);
+                finish();
+            }break;
+
+            // Caso cerrar sesion:
+            case R.id.nav_logout:{
+                ClickCerrarSesion();
+            }
+        }
+        // CIerra el menu despegable al seleccionar alguna opcion
+        dwly.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
